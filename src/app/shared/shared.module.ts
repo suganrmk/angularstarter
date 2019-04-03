e343223da47c1681ed91a8f01e342ff425c9d8dc
sugan <sugan.uideveloper@gmail.com>
2019-04-03 13:06:26 +0530

    structure

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
-import { HeaderComponent } from './components/header/header.component'; 
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component'; 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     HttpClientModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
