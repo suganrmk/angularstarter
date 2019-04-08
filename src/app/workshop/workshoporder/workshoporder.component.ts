dd9f39ed4905e2e2049092b3d9bf1cf342de4bb2
L <karunakaran.l@dxc.com>
2019-04-08 11:08:06 +0530

    icon added

--- src/app/workshop/workshoporder/workshoporder.component.ts
+++ src/app/workshop/workshoporder/workshoporder.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { WorkshoporderService } from '../../core/services/'; 
-
+import { ShareService } from '../../core/services/';
+import { Router } from '@angular/router'
+import { identifierModuleUrl } from '@angular/compiler';
 @Component({
   selector: 'workshop-workshoporder',
   templateUrl: './workshoporder.component.html',
   styleUrls: ['./workshoporder.component.scss']
 })
-
 export class WorkshoporderComponent implements OnInit {
-  trucklistdata: any;  
+  trucklistdata: any;
+  trucklist: any;
+  data: any; 
+  serialNumber: string;
+  shipToPartyNo: string; 
 
-  constructor(private _workshoporderService: WorkshoporderService) { }
+  constructor(private shareservice: ShareService,
+              private _router:Router) { }
 
   ngOnInit() {
-    this._workshoporderService.getAll().subscribe(res =>  this.trucklistdata =  res);
-  } 
+    this.shareservice.postData('http://10.233.36.150:8080/workshopservice/api/rest/trucks?lang=en', {'IV-USER': 'kxdel0095' , 'ecid': 'FOV-FT-TruckListService-Case1'}).subscribe(res => { 
+      console.log(res['trucks'])
+      this.trucklistdata =  res['trucks']; 
+    });
+  }
+  getworkshopordernumber(serialNumber,shipToPartyNo){
+      this._router.navigate(['createworkshoporder/',serialNumber,shipToPartyNo]);
+  }
 }
 
