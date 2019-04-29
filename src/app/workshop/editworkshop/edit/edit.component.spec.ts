83ed0913f2566a77205b0dd98d4f211f005f686d
Shunmugaraj <sshunmugaraj@dxc.com>
2019-04-29 17:05:40 +0530

    testing

--- src/app/workshop/editworkshop/edit/edit.component.spec.ts
+++ src/app/workshop/editworkshop/edit/edit.component.spec.ts
@@ -1,14 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterModule } from '@angular/router';
 import { EditComponent } from './edit.component';
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { EditworkshoporderService, ApiService } from '../../../core/services/';
+import { RouterTestingModule } from '@angular/router/testing';
 describe('EditComponent', () => {
   let component: EditComponent;
   let fixture: ComponentFixture<EditComponent>;
-
+  let _editworkshoporderService: EditworkshoporderService; 
+  let apiService: ApiService;
+  let http: HttpClient;
   beforeEach(async(() => {
+    _editworkshoporderService = new EditworkshoporderService(apiService, http); 
+
     TestBed.configureTestingModule({
-      declarations: [ EditComponent ]
+      declarations: [ EditComponent ],
+      imports: [   RouterModule, HttpClientModule, RouterTestingModule],
+      providers: [EditworkshoporderService, ApiService]
     })
     .compileComponents();
   }));
