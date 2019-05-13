3c1515b17ea31063b95874be3c250cb548fbc824
L <karunakaran.l@dxc.com>
2019-05-13 15:10:57 +0530

    Added routing for side bar icons

--- src/app/workshop/settings/settings-routing.module.ts
+++ src/app/workshop/settings/settings-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SettingComponent } from './setting/setting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: SettingComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/workshop/settings/settings.module.ts
+++ src/app/workshop/settings/settings.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SharedModule } from '../../shared/shared.module';
 import { SettingsRoutingModule } from './settings-routing.module';
+import { SettingComponent } from './setting/setting.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SettingComponent],
   imports: [
     CommonModule,
-    SettingsRoutingModule
+    SettingsRoutingModule,
+    SharedModule,
   ]
 })
 export class SettingsModule { }
