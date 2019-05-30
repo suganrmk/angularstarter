f218be31ab17ffec117202664a025df8d1b4d88a
Shunmugaraj <sshunmugaraj@dxc.com>
2019-05-30 12:11:06 +0530

    removig 0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,4 +9,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes,  { enableTracing: true, useHash:true })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
